db6ed2e39952e144ed272e0ac237aa76a06a22f1
Rohit Tiwari <tigertiwari1023@gmail.com>
2026-02-18 21:59:35 +0530

    add : browser client with the ocpp socket and typing support

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -1,10 +1,20 @@
 import { source } from "@/lib/source";
-import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
+  const { ...options } = baseOptions();
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      {...options}
+      tree={source.getPageTree()}
+      sidebar={{
+        enabled: true,
+        collapsible: false,
+      }}
+      tabMode="top"
+      githubUrl="https://github.com/rohittiwari-dev/ocpp-ws-io"
+    >
       {children}
     </DocsLayout>
   );
