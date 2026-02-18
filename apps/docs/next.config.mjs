88e565bcf25a922694ef295e62beebf417dfb06a
Rohit Tiwari <tigertiwari1023@gmail.com>
2026-02-18 21:59:35 +0530

    add : browser client with the ocpp socket and typing support

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -12,6 +12,7 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "export",
   async rewrites() {
     return [
       {
--- apps/docs/src/lib/source.ts
+++ apps/docs/src/lib/source.ts
@@ -1,12 +1,20 @@
 import { docs } from "fumadocs-mdx:collections/server";
 import { type InferPageType, loader } from "fumadocs-core/source";
 import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
+import { createElement } from "react";
+import { icons } from "lucide-react";
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
   plugins: [lucideIconsPlugin()],
+  icon(icon) {
+    if (!icon) {
+      return;
+    }
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
