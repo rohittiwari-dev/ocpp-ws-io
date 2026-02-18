5a6cf9e2cdb891031623a9e840d7acf24d371433
Rohit Tiwari <tigertiwari1023@gmail.com>
2026-02-19 02:59:14 +0530

    add: logger package

---
+++ packages/ocpp-ws-io/src/logger/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @module ocpp-ws-io/logger
+ *
+ * Re-exports the `voltlog` package for convenience.
+ *
+ * Users who already have `ocpp-ws-io` installed can use:
+ * ```ts
+ * import { createLogger } from 'ocpp-ws-io/logger';
+ * ```
+ *
+ * Instead of installing `voltlog` separately.
+ */
+export * from "voltlog";
