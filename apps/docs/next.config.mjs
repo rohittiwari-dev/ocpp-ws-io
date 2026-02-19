9b4f5ad4f22edf914674ea12d20385592c8523be
Rohit Tiwari <tigertiwari1023@gmail.com>
2026-02-19 20:26:34 +0530

    add: blogs (#9)

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -12,6 +12,9 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
