d878e50115c4b78b54327d88b850c7b733907f04
Rohit Tiwari <tigertiwari1023@gmail.com>
2026-02-28 07:18:07 +0530

    fix: docs version [skip ci]

--- apps/docs/src/app/og/docs/[...slug]/route.tsx
+++ apps/docs/src/app/og/docs/[...slug]/route.tsx
@@ -1,7 +1,7 @@
-import { generate as DefaultImage } from "fumadocs-ui/og";
+import { getPageImage, source } from "@/lib/source";
 import { notFound } from "next/navigation";
 import { ImageResponse } from "next/og";
-import { getPageImage, source } from "@/lib/source";
+import { generate as DefaultImage } from "fumadocs-ui/og";
 
 export const revalidate = false;
 
@@ -14,11 +14,13 @@ export async function GET(
   if (!page) notFound();
 
   return new ImageResponse(
-    <DefaultImage
-      title={page.data.title}
-      description={page.data.description}
-      site="My App"
-    />,
+    (
+      <DefaultImage
+        title={page.data.title}
+        description={page.data.description}
+        site="My App"
+      />
+    ),
     {
       width: 1200,
       height: 630,
